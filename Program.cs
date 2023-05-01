3524fdff61b1abfca8a0ae51ad1823e16b87e131
Chorn4 <118160241+Chorn4@users.noreply.github.com>
2023-05-02 00:38:47 +0300

    Код від завдання 5

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Alfred bought the apples but Alfred forgot that Alfred sold the apples";
+            string [] words = sentence.Split(' ');
+
+            Console.WriteLine(sentence);
+            Console.WriteLine("\nWords that appear only once:");
+
+            foreach (string word in words)
+            {
+                if (sentence.IndexOf(word) == sentence.LastIndexOf(word))
+                {
+                    Console.WriteLine("-" + word);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
